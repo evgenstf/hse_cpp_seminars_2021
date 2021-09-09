1409f473c2a3d7a7503358d2354dd8c66d478905
Evgenii Kazakov <evgenstf@gmail.com>
2021-09-09 09:19:38 +0000

    fix seminar_20210909 sample_3 cmake

--- seminar_20210909/cpp_linking/sample_4/main.cpp
+++ seminar_20210909/cpp_linking/sample_4/main.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
+
 double average(int a, int b);
 
 int main() {
   int a = 10;
   int b = 20;
-  return static_cast<int>(average(a, b));
+  std::cout << static_cast<int>(average(a, b)) << std::endl;
+  return 0;
 }
