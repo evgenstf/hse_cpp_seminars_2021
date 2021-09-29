1a32c662fa8d6d6fb68296a5bfb9b93699946732
Evgenii Kazakov <evgenstf@gmail.com>
2021-09-29 22:01:30 +0100

    add seminar_20210930

---
+++ seminar_20210930/inheritance/examples.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+
+
+using namespace std;
+
+
+class ParentA {
+public:
+  int a = 0xaaaaaaaa;
+};
+
+class ChildB : public ParentA {
+public:
+  int b = 0xbbbbbbbb;
+};
+
+
+class ChildC : public ParentA {
+public:
+  int c = 0xcccccccc;
+};
+
+
+int main() {
+  ParentA a;
+  ChildB b;
+  ChildC c;
+
+  {
+    ofstream file;
+    file.open("parent_a.bin", ios::out | ios::binary);
+    file.write(reinterpret_cast<char*>(&a), sizeof(a));
+  }
+
+  {
+    ofstream file;
+    file.open("parent_b.bin", ios::out | ios::binary);
+    file.write(reinterpret_cast<char*>(&b), sizeof(b));
+  }
+
+  {
+    ofstream file;
+    file.open("parent_c.bin", ios::out | ios::binary);
+    file.write(reinterpret_cast<char*>(&c), sizeof(c));
+  }
+}
